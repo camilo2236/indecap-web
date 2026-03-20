e8968e172b5814b3c4e1f8ba78a784d48d1c3a63
camilo2236 <camilo2236@gmail.com>
2026-03-19 23:17:54 -0500

    fix: sistema de sedes y contactos unificado

--- src/config/contacts.ts
+++ src/config/contacts.ts
@@ -1,7 +1,7 @@
 export const CONTACTS = {
-  CURSOS: "573167405680",   // Línea exclusiva para Cursos Cortos y Diplomados
-  ENVIGADO: "573174342783", // Sede Envigado (Línea principal)
+  CURSOS: "573167405680",   // Vanesa (Cursos Cortos)
+  ENVIGADO: "573174342783", // Sede Envigado
   MEDELLIN: "573022389760", // Sede Medellín
   CALDAS: "573008948517",   // Sede Caldas
-  GENERAL: "573174342783"   // Respaldo
+  PRINCIPAL: "573174342783" 
 };
